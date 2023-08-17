c2e8bbc646ad12cda5e1add00c1b67b4cd9b2e3e
Tao Chen <taochensyd@gmail.com>
2023-08-18 09:40:31 +1000

    add validate logic and display error when encounter error

--- PowerShell/FindFilePath.ps1
+++ PowerShell/FindFilePath.ps1
@@ -1,9 +1,12 @@
-$filePath = Read-Host -Prompt "Enter the file path"
-$fileName = Read-Host -Prompt "Enter the file name"
+try {
+    $filePath = Read-Host -Prompt "Enter the file path"
+    $fileName = Read-Host -Prompt "Enter the file name"
 
-Get-ChildItem -Path $filePath -Filter "*$fileName*" -Recurse | ForEach-Object {
-    Write-Output $_.FullName
+    Get-ChildItem -Path $filePath -Filter "*$fileName*" -Recurse | ForEach-Object {
+        Write-Output $_.FullName
+    }
+} catch {
+    Write-Host "An error occurred: $($_.Exception.Message)"
 }
-
 
 Read-Host -Prompt "Press Enter to exit"
